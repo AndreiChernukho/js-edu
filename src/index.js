77697731ba2095ca1fe9cff24cd53c55f192de93
sol2101 <sol2101@yandex.ru>
2019-10-05 15:04:25 +0300

    feat: implement the getTimeForEducation function

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+  focus = 'family',
+  knowsProgramming = true,
+  config = { family: 4 }
+) {
+  let time = knowsProgramming ? 800 : 1300;
+  let week = Math.ceil(time/config[focus]);
+  return week;
+};
